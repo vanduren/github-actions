349cec2ac01438adb115d197f792a2ae3c2fee5f
dujo <dujo@summacollege.nl>
2024-12-11 21:18:36 +0100

    another stupid ...

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -14,6 +14,7 @@ class ProductController extends Controller
     {
         // stupid comment
         // another stupid comment
+        // another stupid comment
         $products = Product::all();
         return view('products.index', compact('products'));
     }
